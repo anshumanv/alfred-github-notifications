34379ccdce44005c649ea2a7250cc6c9851f01c3
Anshuman Verma <anshu.av97@gmail.com>
2018-12-25 13:08:11 +0530

    Add support for all and participating flags

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const alfy = require('alfy');
 
 // Script to run on changing input
 (async () => {
-
-	const data = await alfy.fetch(`https://api.github.com/notifications?access_token=${process.env.access_token}`)
+	const flags = alfy.input.split(' ');
+	const all = flags.includes('a')
+	const participating = flags.includes('p')
+	const apiLink = `https://api.github.com/notifications?access_token=${process.env.access_token}${all ? '&all=1' : ''}${participating ? '&participating=1' : ''}`
+	const data = await alfy.fetch(apiLink)
 	const items =	data.map(event => {
 		
 		const targetUrl = event.subject.url.split('/').splice(4).join('/');
